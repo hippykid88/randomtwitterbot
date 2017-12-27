09f27a62a5a8bba84e2958359ba138c314b10790
Cory <rocky@corysmith.xyz>
2017-12-27 14:55:14 -0700

    changed logic

---
+++ config-sample.py
@@ -0,0 +1,12 @@
+CONSUMER_KEY = 'Place Key Here'
+CONSUMER_SECRET = 'Place Key Here'
+ACCESS_KEY = 'Place Key Here'
+ACCESS_SECRET = 'Place Key Here'
+
+
+"""Here is the list of tweets and their photo associated with it"""
+OPTION = []
+OPTION.append( {"media": "photos/burger.jpg", "tweet_text": "Burgers, we have the biggest in town up to 2lbs!!!"} )
+OPTION.append( {"media": "photos/French_Toast.jpeg", "tweet_text": "Famous French Toast (Customer favorite) starting at $7.25"} )
+OPTION.append( {"media": "photos/Prime_Rib.jpeg", "tweet_text": "Prime Ribs only $8.95 everyday!"} )
+OPTION.append( {"media": "photos/Omelettes.jpeg", "tweet_text": "Omeletts starting at $7.99"} )
